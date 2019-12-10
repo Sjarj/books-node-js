bf340548cb8eb435a3dd30ce5a698df726fb8027
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-10 19:29:27 +0100

    Assert and done

--- test/connexion_test.js
+++ test/connexion_test.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
 
 before(done => {
   mongoose.connect(
-    'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority',
+    "mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority",
     { useUnifiedTopology: true, useNewUrlParser: true }
   );
   mongoose.connection
-    .once('open', () => {
-      console.log('Connexion est établie', done());
+    .once("open", () => {
+      console.log("Connexion est établie", done());
     })
-    .on('error', error => {
-      console.warn('Erreur durant la connexion', error);
+    .on("error", error => {
+      console.warn("Erreur durant la connexion", error);
     });
 });
 
-beforeEach('Supprime les anciens livres', done => {
+beforeEach("Supprime les anciens livres", done => {
   const { books } = mongoose.connection.collections;
   books.drop(() => done());
 });
--- test/create_test.js
+++ test/create_test.js
@@ -1,9 +1,12 @@
-const assert = require('assert');
-const Book = require('../src/books');
+const assert = require("assert");
+const Book = require("../src/books");
 
-describe('Création de livre', () => {
-  it('Sauvegarde d un livre ', () => {
-    const book1 = new Book({ title: 'Harry Potter' });
-    book1.save();
+describe("Création de livre", () => {
+  it("Sauvegarde d un livre ", done => {
+    const book1 = new Book({ title: "Harry Potter" });
+    book1.save().then(() => {
+      assert(!book1.isNew);
+      done();
+    });
   });
 });
