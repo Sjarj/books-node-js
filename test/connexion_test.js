b0733f74bff0840c14bc3ae05138c8361c137742
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-10 17:52:20 +0100

    Connect server

---
+++ test/connexion_test.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority',
+  { useUnifiedTopology: true }
+);
+mongoose.connection
+  .once('open', () => {
+    console.log('Connexion est établie');
+  })
+  .on('error', error => {
+    console.warn('Erreur durant la connexion', error);
+  });
