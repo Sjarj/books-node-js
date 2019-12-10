05a29eecd1a3cf4e918b3cabffdcdd3d68c72937
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-10 18:44:58 +0100

    Clean up database each test

--- test/connexion_test.js
+++ test/connexion_test.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority',
-  { useUnifiedTopology: true, useNewUrlParser: true }
-);
-mongoose.connection
-  .once('open', () => {
-    console.log('Connexion est établie');
-  })
-  .on('error', error => {
-    console.warn('Erreur durant la connexion', error);
-  });
+before(done => {
+  mongoose.connect(
+    'mongodb+srv://Sjarj:lesilence@cluster0-l1jes.mongodb.net/test?retryWrites=true&w=majority',
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  );
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connexion est établie', done());
+    })
+    .on('error', error => {
+      console.warn('Erreur durant la connexion', error);
+    });
+});
+
+beforeEach('Supprime les anciens livres', done => {
+  const { books } = mongoose.connection.collections;
+  books.drop(() => done());
+});
