1a1794deb1c6a47f1a2c1cc94cb805cf5a42d842
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-16 18:47:20 +0100

    Midlleware

--- src/users.js
+++ src/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const BookSchema = require('./books').schema;
+const BlogBook = require('./blogBooks');
 
 const UserSchema = new Schema({
   name: String,
@@ -11,6 +12,10 @@ const UserSchema = new Schema({
 
 UserSchema.virtual('countBooks').get(function() {
   return this.books.length;
+});
+
+UserSchema.pre('remove', function(next) {
+  BlogBook.remove({ _id: { $in: this.blogBooks } }).then(() => next());
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/reference_test.js
+++ test/reference_test.js
@@ -4,35 +4,37 @@ const User = require('../src/users');
 const BlogBook = require('../src/blogBooks');
 const Comment = require('../src/comments');
 
-describe('Test de reference', () => {
+describe('Test de references', () => {
+  let robin, blogBook, comment;
+
   beforeEach(done => {
-    let user, blogBook, comment;
-
-    user = new User({ name: `Robin` });
+    robin = new User({ name: 'Robin' });
     blogBook = new BlogBook({
-      title: `Les fourmis`,
-      sumary: `Les fourmis: un livre qui concerne les fourmis...`
+      title: 'Les fourmis',
+      summary: 'Les fourmis : un livre qui concerne, les fourmis...'
     });
-    comment = new Comment({ content: `j'adore les fourmis !` });
+    comment = new Comment({ content: 'Wao génial super comme livre !' });
 
-    user.blogBooks.push(blogBook);
+    robin.blogBooks.push(blogBook);
     blogBook.comments.push(comment);
-    comment.user = user;
+    comment.user = robin;
 
-    Promise.all([user.save(), blogBook.save(), comment.save()]).then(() =>
-      done()
-    );
+    Promise.all([robin.save(), blogBook.save(), comment.save()]).then(() => {
+      done();
+    });
   });
 
-  it(`Test le titre du livre d'un user`, done => {
-    User.findOne({ name: `Robin` })
+  it('Test les le titre du livre d un user ', done => {
+    User.findOne({ name: 'Robin' })
       .populate('blogBooks')
-      .then(user => assert(user.blogBooks[0].title === 'Les fourmis'));
-    done();
+      .then(user => {
+        assert(user.blogBooks[0].title == 'Les fourmis');
+        done();
+      });
   });
 
-  it(`Test pour trouver le commentaire d'un livre`, done => {
-    User.findOne({ name: `Robin` })
+  it('Test le commentaire du livre d un user ', done => {
+    User.findOne({ name: 'Robin' })
       .populate({
         path: 'blogBooks',
         populate: {
@@ -40,11 +42,12 @@ describe('Test de reference', () => {
           model: 'comment'
         }
       })
-      .then(user =>
+      .then(user => {
         assert(
-          user.blogBooks[0].comments[0].content === `j'adore les fourmis !`
-        )
-      );
-    done();
+          user.blogBooks[0].comments[0].content ===
+            'Wao génial super comme livre !'
+        );
+        done();
+      });
   });
 });
